ec6d3c891c878452189def3d244f24d0769d915b
Thor Kummer <tk@one.com>
2016-12-05 01:29:39 +0100

    Sleeping for 60 sec to keep main thread alive instead of using semaphore.

--- src/main/groovy/me/kummer/firebase/Main.groovy
+++ src/main/groovy/me/kummer/firebase/Main.groovy
@@ -2,20 +2,14 @@ package me.kummer.firebase
 
 import com.firebase.client.ChildEventListener
 import com.firebase.client.DataSnapshot
-
-//import com.firebase.client
 import com.firebase.client.Firebase
 import com.firebase.client.FirebaseError
 import com.firebase.client.Query
 import com.firebase.client.ValueEventListener
-import java.util.concurrent.Semaphore
 
 class Main {
 
     static void main(args) {
-
-        def semaphore = new Semaphore(1)
-        semaphore.acquire()
 
         Firebase rootRef = new Firebase("https://${('firebase_' as File).text}.firebaseio.com/");
 
@@ -25,31 +19,26 @@ class Main {
             @Override
             void onChildAdded(DataSnapshot snapshot, String previousChild) {
                 println "Added: $snapshot.key: $snapshot.value"
-                semaphore.release()
             }
 
             @Override
             void onChildChanged(DataSnapshot snapshot, String s) {
                 println "Changed: $snapshot.key: $snapshot.value"
-                semaphore.release()
             }
 
             @Override
             void onChildRemoved(DataSnapshot snapshot) {
                 println "Removed: $snapshot.key: $snapshot.value"
-                semaphore.release()
             }
 
             @Override
             void onChildMoved(DataSnapshot snapshot, String s) {
                 println "Moved: $snapshot.key: $snapshot.value"
-                semaphore.release()
             }
 
             @Override
             void onCancelled(FirebaseError error) {
                 println "Cancelled: $error"
-                semaphore.release()
             }
         });
 
@@ -58,17 +47,17 @@ class Main {
             @Override
             void onDataChange(DataSnapshot snapshot) {
                 println(snapshot.getValue());
-                semaphore.release()
             }
+
             @Override
             void onCancelled(FirebaseError firebaseError) {
-                println("The read failed: " + firebaseError.getMessage());
-                semaphore.release()
+                println "The read failed: ${firebaseError.message}"
             }
         });
 
-        println "Aquiring semaphore"
-        semaphore.acquire()
+        Thread.sleep 60000
+
+        println "Exiting."
     }
 
 }
